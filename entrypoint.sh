0d03cd0b22e19b98a907eaaf66eea9177368a1e6
penguin_jan <penguin_jan@mail.chungyo.net>
2024-10-31 10:36:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,25 +17,19 @@ if [ -z != ${10} ]; then
 	echo 'use sshpass'
 	apk add sshpass
 
-	if test $9 == "true";then
-  		echo 'Start delete remote files'
-		sshpass -p ${10} ssh -o StrictHostKeyChecking=no -p $3 $1@$2 rm -rf $6
+	# Create an SFTP batch file with mkdir command if directory creation is required
+	if [ "$7" != "true" ]; then
+		echo 'Create directory if needed'
+		printf "%s\n" "mkdir $6" >$TEMP_SFTP_FILE
 	fi
-	if test $7 = "true"; then
-  		echo "Connection via sftp protocol only, skip the command to create a directory"
-	else
- 	 	echo 'Create directory if needed'
- 	 	sshpass -p ${10} ssh -o StrictHostKeyChecking=no -p $3 $1@$2 mkdir -p $6
-	fi
-
+	
+	# Add the put command to transfer files
 	echo 'SFTP Start'
-	# create a temporary file containing sftp commands
-	printf "%s" "put -r $5 $6" >$TEMP_SFTP_FILE
-	#-o StrictHostKeyChecking=no avoid Host key verification failed.
+	printf "%s\n" "put -r $5 $6" >>$TEMP_SFTP_FILE
+	# Execute SFTP batch file using sshpass with password
 	SSHPASS=${10} sshpass -e sftp -oBatchMode=no -b $TEMP_SFTP_FILE -P $3 $8 -o StrictHostKeyChecking=no $1@$2
 
 	echo 'Deploy Success'
-
     exit 0
 fi
 
@@ -44,24 +38,18 @@ printf "%s" "$4" >$TEMP_SSH_PRIVATE_KEY_FILE
 # avoid Permissions too open
 chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE
 
-# delete remote files if needed
-if test $9 == "true";then
-  echo 'Start delete remote files'
-  ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 rm -rf $6
-fi
-
-if test $7 = "true"; then
-  echo "Connection via sftp protocol only, skip the command to create a directory"
-else
+# Create an SFTP batch file with mkdir command if directory creation is required
+if [ "$7" != "true" ]; then
   echo 'Create directory if needed'
-  ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 mkdir -p $6
+  printf "%s\n" "mkdir $6" >$TEMP_SFTP_FILE
 fi
 
+# Add the put command to transfer files
 echo 'SFTP Start'
-# create a temporary file containing sftp commands
-printf "%s" "put -r $5 $6" >$TEMP_SFTP_FILE
-#-o StrictHostKeyChecking=no avoid Host key verification failed.
-sftp -b $TEMP_SFTP_FILE -P $3 $8 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2
+printf "%s\n" "put -r $5 $6" >>$TEMP_SFTP_FILE
+
+# Execute SFTP batch file using private key authentication
+sftp -b $TEMP_SFTP_FILE -P $3 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2
 
 echo 'Deploy Success'
 exit 0
